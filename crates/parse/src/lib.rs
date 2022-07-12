b2ee4b723fd157bf4b592e58351719cde4e37631
BJogart <jasper.opti@gmail.com>
2022-07-12 16:13:06 +0200

    set up parser api

--- crates/parse/src/lib.rs
+++ crates/parse/src/lib.rs
@@ -1,14 +1,18 @@
 use query_base::QueryBase;
-use source::Src;
+use syntax::SyntaxKind;
 
 pub trait Queries: QueryBase {
-    fn parse<B: Builder>(self, src: Src, builder: B) -> B::Out {
-        todo!()
+    fn parse<B: Builder>(self, builder: &mut B, tokens: Vec<(usize, SyntaxKind, usize)>) {
     }
 }
 
-pub enum Error {}
-
 pub trait Builder {
     type Out;
+    type Error;
+
+    fn start(&mut self, kind: SyntaxKind);
+    fn end(&mut self);
+    fn token(&mut self);
+    fn error(&mut self);
+    fn build(self) -> (Self::Out, Vec<Self::Error>);
 }
