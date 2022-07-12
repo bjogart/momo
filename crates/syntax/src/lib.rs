4ea5581fdcd09b1169bfd9741d7ab1bd5ae2effc
BJogart <jasper.opti@gmail.com>
2022-07-12 16:15:19 +0200

    add some token utility functions used for AST constr

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -1,13 +1,20 @@
 use smol_str::SmolStr;
 
+#[derive(Clone, Debug)]
 pub struct Token {
     kind: SyntaxKind,
     s: SmolStr,
 }
 
 impl Token {
-    pub fn new(kind: SyntaxKind, s: String) -> Self {
-        Self { kind, s: SmolStr::new(&s) }
+    pub fn new(kind: SyntaxKind, s: &str) -> Self {
+        Self { kind, s: SmolStr::new(s) }
+    }
+
+    pub fn len(&self) -> usize  { self.s.len()}
+
+    pub fn kind(&self) -> SyntaxKind {
+        self.kind
     }
 }
 
