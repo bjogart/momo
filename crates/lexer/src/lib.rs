38fbf11797b96bc538ad1eb527c067353e0d7f75
BJogart <jasper.opti@gmail.com>
2022-07-12 16:20:24 +0200

    add (maybe necy) commented code from prev impl

--- crates/lexer/src/lib.rs
+++ crates/lexer/src/lib.rs
@@ -13,6 +13,7 @@ pub trait Queries: QueryBase {
         let pos = &mut 0;
         text.chars()
             .batching(|it| scan_next_token(text, pos, it))
+            // .map(adjust_idents_to_keywords)
             .collect_vec()
     }
 }
@@ -51,17 +52,57 @@ fn scan_next_token(text: &str, pos: &mut usize, it: &mut Chars<'_>) -> Option<To
                 SyntaxKind::NL
             }
             '=' => {
+            //     if let Some(n_consumed) = eat_if(it, |ch| *ch == '=') {
+            //         len += n_consumed;
+            //         SyntaxKind::EQEQ
+            //     } else {
                     SyntaxKind::EQ
+            //     }
+            }
+            // '|' => {
+            //     if let Some(n_consumed) = eat_if(it, |ch| *ch == '|') {
+            //         len += n_consumed;
+            //         SyntaxKind::BARBAR
+            //     } else {
+            //         SyntaxKind::BAR
+            //     }
+            // }
+            // '+' => SyntaxKind::PLUS,
+            // '-' => {
+            //     if let Some(n_consumed) = eat_if(it, |ch| *ch == '>') {
+            //         len += n_consumed;
+            //         SyntaxKind::ARROW
+            //     } else {
+            //         SyntaxKind::MINUS
+            //     }
+            // }
+            // '*' => SyntaxKind::STAR,
+            // ';' => SyntaxKind::SEMI,
             '(' => SyntaxKind::LPAR,
             ')' => SyntaxKind::RPAR,
             ':' => SyntaxKind::COLON,
+            // '/' => {
+            //     if let Some(n_consumed) = eat_if(it, |ch| *ch == '/') {
+            //         len += n_consumed + eat_while(it, |ch| *ch != '\n');
+            //         SyntaxKind::COMMENT
+            //     } else {
+            //         SyntaxKind::SLASH
+            //     }
+            // }
+            // '[' => SyntaxKind::LBRAC,
+            // ']' => SyntaxKind::RBRAC,
             ',' => SyntaxKind::COMMA,
+            // '{' => SyntaxKind::LCURL,
+            // '}' => SyntaxKind::RCURL,
+            // '_' => SyntaxKind::UDASH,
             _ => SyntaxKind::ERR_UNEXPECTED_CHAR,
+        };
+
         let t = Token::new(kind, &text[*pos..*pos + len]);
         *pos += len;
         t
     })
-            }
+}
 
 impl LexChar for char {
     fn is_ident_start(&self) -> bool {
@@ -80,6 +121,27 @@ impl LexChar for char {
         matches!(self, '0'..='9')
     }
 }
+
+// fn adjust_idents_to_keywords(t: Token) -> Token {
+//     match t.kind() {
+//         SyntaxKind::IDENT => {
+//             let kind = match t.string.as_str() {
+//                 "data" => Some(SyntaxKind::DAT),
+//                 "mod" => Some(SyntaxKind::MODULE),
+//                 "case" => Some(SyntaxKind::KASE),
+//                 "use" => Some(SyntaxKind::UZE),
+//                 "as" => Some(SyntaxKind::AZ),
+//                 _ => None,
+//             };
+
+//             match kind {
+//                 Some(kind) => t.with_kind(kind),
+//                 None => t,
+//             }
+//         }
+//         _ => t,
+//     }
+// }
 
 fn eat(it: &mut Chars<'_>) -> Option<(char, usize)> {
     it.next().map(|ch| (ch, ch.len_utf8()))
