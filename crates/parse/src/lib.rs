8d45bf81c6dcaa6319f6565080137783a180b931
BJogart <jasper.opti@gmail.com>
2022-07-12 16:13:27 +0200

    set up skeleton for lalrpop parser

--- crates/parse/src/lib.rs
+++ crates/parse/src/lib.rs
@@ -1,8 +1,15 @@
+use lalrpop_util::lalrpop_mod;
 use query_base::QueryBase;
 use syntax::SyntaxKind;
 
+lalrpop_mod!(parser);
+
 pub trait Queries: QueryBase {
     fn parse<B: Builder>(self, builder: &mut B, tokens: Vec<(usize, SyntaxKind, usize)>) {
+        match parser::FileParser::new().parse(builder, tokens.into_iter()) {
+            Ok(_) => {}
+            Err(err) => panic!("lalrpop parser error: `{err:?}`"),
+        }
     }
 }
 
